bcb5119cf05dd9ec9738ef9a661577456b27d60c
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2019-11-11 12:30:36 +0100

    Step 1

---
+++ src/Model.elm
@@ -0,0 +1,51 @@
+module Model exposing (..)
+
+import Browser exposing (Document,application)
+import Browser.Navigation as Nav
+import Monocle.Lens exposing (Lens)
+import Routing exposing (..)
+import Uuid exposing (Uuid, uuidGenerator)
+import Random exposing (Seed, step, initialSeed)
+import Question.Model exposing (..)
+import Question.Optics exposing (..)
+import Question.QuestionDetailsView exposing (..)
+import Question.OptionDetailsView exposing(..)
+import QuestionCategory.Model exposing (..)
+import QuestionCategory.Optics exposing (..)
+import QuestionCategory.DetailsView exposing (..)
+import Optics exposing (..)
+import JsonModel.DetailsView exposing (..)
+import QuestionTemplate.Model exposing (..)
+import QuestionTemplate.Views exposing (..)
+import QuestionTemplate.Optics exposing (..)
+import Url exposing (Url)
+import Menu exposing (..)
+
+-- Types
+
+type alias Model = { route : Route, key : Nav.Key, uuidSeed : Seed, questionTemplate : QuestionTemplate, menuState : MenuState }
+
+type Msg = UpdateModel (Model -> Model)
+         | ChangedUrl Url
+         | ClickedLink Browser.UrlRequest
+         | MenuAction MenuMsg
+         | QuestionTemplateLoaded QuestionTemplate
+         | QuestionTemplateSaved
+         | NoAction
+
+type SelectedEntity a = Question (Focus a Question)
+                      | Option (Focus a QuestionOption)
+                      | Category (Focus a QuestionCategory )
+                      | None
+
+-- Id generation
+
+type alias Seedable a = { a | uuidSeed : Seed }
+
+withNewId : (String -> Seedable a -> Seedable a) -> Seedable a -> Seedable a
+withNewId f model =
+            let
+                (newUuid, newSeed) =
+                    step uuidGenerator model.uuidSeed
+            in
+                f (Uuid.toString newUuid) { model | uuidSeed = newSeed } 
